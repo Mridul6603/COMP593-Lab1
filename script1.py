425ebd8777d1c40a73e1a843def3cd4a0586949c
Mridul6603 <108378044+Mridul6603@users.noreply.github.com>
2022-07-11 17:20:37 -0400

    Revert "Created Script files"
    This reverts commit d6dd9bd3f65e22242a32be9e20575e60206ae087.

--- script1.py
+++
@@ -1,3 +0,0 @@
-print("Errors should never pass silently.")
-print("Unless explicitly silenced.")
-
--- script2.py
+++
@@ -1,2 +0,0 @@
-print("Now is better than never.")
-print("Although never is often better than *right* now.")
