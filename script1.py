d78d6b6298d7b20952e61eee0f4d0776494a44e4
Mridul6603 <108378044+Mridul6603@users.noreply.github.com>
2022-07-14 16:54:45 -0400

    Modified both script files
    -Deleted print from script1.py
    -Added comment to script2.py

---
+++ script1.py
@@ -0,0 +1,1 @@
+print("Errors should never pass silently.")
---
+++ script2.py
@@ -0,0 +1,3 @@
+#This is a good programming language
+print("Now is better than never.")
+print("Although never is often better than *right* now.")
